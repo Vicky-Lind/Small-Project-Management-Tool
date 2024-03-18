27d74557659a58c62607f79be1c1aa49c3506603
Vicky-Lind <112470720+Vicky-Lind@users.noreply.github.com>
2024-03-18 14:14:22 +0200

    fixed Task.deadline

--- projects/models.py
+++ projects/models.py
@@ -32,7 +32,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.NOT_STARTED)
     priority = models.CharField(max_length=20, choices=PriorityChoices.choices, default=PriorityChoices.MEDIUM)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
